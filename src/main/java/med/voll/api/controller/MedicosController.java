038a1ba763e863c0c45d7dc9bd8a0b7e6298090e
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-06-26 13:30:49 -0300

    listar medicos - metodo get

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -4,13 +4,13 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -26,5 +26,11 @@ public class MedicosController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         //pegar o repository e fazer persistir no banco de dados
         repository.save(new Medico(dados));
+    }
+
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
