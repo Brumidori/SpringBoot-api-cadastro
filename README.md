9595d03198525c058354eb6f765e2ef44237eb6a
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-06-26 10:48:07 -0300

    mapear a entidade Jpa pacientes, criar repository e utilizar o flyway como migration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SpringBoot-api-crud-cadastro
 Projeto desenvolvido em Java utilizando SpringBoot3 para construção de uma API de cadastro com CRUD completo e conexão com MySQL
+
+Mapear uma entidade JPA e criar uma interface Repository para ela;
+Utilizar o Flyway como ferramenta de Migrations do projeto;
+Realizar validações com Bean Validation utilizando algumas de suas anotações, como a @NotBlank.
--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.pacientes.DadosCadastroPaciente;
+import med.voll.api.pacientes.Paciente;
+import med.voll.api.pacientes.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
-    }
+    @Autowired
+    private PacienteRepository repository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
 }
