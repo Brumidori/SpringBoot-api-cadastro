afeedc1056554868161c6fd4cf903336bf2a35e2
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-18 14:01:03 -0300

    Tratamento erro 400 - mensagem personalizada

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +14,17 @@ public class TratadorDeErros {
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e ){
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao:: new).toList());
+    }
+
+    public record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
