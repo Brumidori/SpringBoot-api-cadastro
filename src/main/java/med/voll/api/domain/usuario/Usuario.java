1b3dd0d71f737db081158b4ef8018ee5bc55bf82
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-19 16:15:38 -0300

    entity usuario + migration

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
