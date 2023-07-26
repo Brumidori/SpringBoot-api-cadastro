7c87f239b353541e7603db75906f2c80bb3fb81f
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-26 10:42:22 -0300

    adicionando token da biblioteca JWT e configurando

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJwt;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,10 +21,23 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
-        return  ResponseEntity.ok().build();
+        //Esse primeiro token é o login e a senha, e já está sendo representado no DTO DadosAutenticacao. No entanto,
+        // esse DTO não é o parâmetro esperado pelo Spring, ele espera uma classe dele próprio -
+        // e não uma classe do projeto.
+        //Portanto, na variável token criaremos a classe que representa o usuário e a senha.
+        // Após o new, vamos instanciar um objeto do tipo UsernamePasswordAuthenticationToken()
+        // passando como parâmetro o DTO, sendo dados.login(), e dados.senha().
+
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        //devolve um token gerado pelo JWT token em um DTO json
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return  ResponseEntity.ok(new DadosTokenJwt(tokenJWT));
     }
 }
