d6c9c1ed35404cf82a53333802dc51795b7353a1
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-27 18:30:11 -0300

    classes de validação cancelamento

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorCancelamentoDeConsulta.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.consulta.validacoes.cancelamento;
+
+
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+
+public interface ValidadorCancelamentoDeConsulta {
+
+    void validar(DadosCancelamentoConsulta dados);
+
+}
