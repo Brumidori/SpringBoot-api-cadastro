0a7ac850bb17036bb10967fbc2fceb353844c2a1
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-07-27 16:14:17 -0300

    logica cancelamento de consulta

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -30,7 +30,7 @@ public class AgendaDeConsultas {
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var medico = escolherMedico(dados);
 
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
     }
 
@@ -42,4 +42,13 @@ public class AgendaDeConsultas {
             throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!");
         }
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
+    }
 }
